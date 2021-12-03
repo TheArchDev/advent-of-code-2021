376eb2e9ab9e5f2f2473869d42cca70bae87bacc
Alex Archer <archeralex@gmail.com>
2021-12-03 12:58:52 +0000

    Day 1: include base logic to read file defined by flag line by line and print

--- day1/day1.go
+++ day1/day1.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	filePtr := flag.String("file", "input.txt", "input file location")
+	flag.Parse()
+
+	file, err := os.Open(*filePtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	var text []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+		text = append(text, line)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(text)
 }
