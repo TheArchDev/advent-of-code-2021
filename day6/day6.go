3fe71bbb428ae2a5489c7bbcf785a93e0700d593
Alex Archer <archeralex@gmail.com>
2021-12-06 15:32:58 +0000

    Day 6: implementation for part 1; actually include input.txt

--- day6/day6.go
+++ day6/day6.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	// "bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
-func main(){
-	fmt.Println("Hello, world")
+func main() {
+	filePtr := flag.String("file", "input.txt", "input file location")
+	daysPtr := flag.Int("days", 80, "number of days to simulate")
+	flag.Parse()
+
+	fileContent, err := os.ReadFile(*filePtr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	inputs := strings.Split(string(fileContent), ",")
+	timers := make([]int, len(inputs))
+	for index, timer := range inputs {
+		timers[index], _ = strconv.Atoi(timer)
+	}
+
+	for i := 1; i <= *daysPtr; i++ {
+		var newTimers []int
+		for index, timer := range timers {
+			if timer == 0 {
+				timer = 6
+				newTimers = append(newTimers, 8)
+			} else {
+				timer--
+			}
+			timers[index] = timer
+		}
+		timers = append(timers, newTimers...)
+	}
+	fmt.Println("Number of lanterns", len(timers))
 }
