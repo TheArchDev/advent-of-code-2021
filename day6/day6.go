6761ab1423e302c78acd06972597c3195c92a76d
Alex Archer <archeralex@gmail.com>
2021-12-06 17:04:06 +0000

    Day 6: refactor to be more performant as part of solving part 2

--- day6/day6.go
+++ day6/day6.go
@@ -10,6 +10,17 @@ import (
 	"strings"
 )
 
+func calculate_timers(timerCounts map[int]int) map[int]int {
+	updatedCounts := make(map[int]int)
+	for i := 0; i < 8; i++ {
+		updatedCounts[i] = timerCounts[i+1]
+	}
+	updatedCounts[6] += timerCounts[0]
+	updatedCounts[8] = timerCounts[0]
+
+	return updatedCounts
+}
+
 func main() {
 	filePtr := flag.String("file", "input.txt", "input file location")
 	daysPtr := flag.Int("days", 80, "number of days to simulate")
@@ -19,25 +30,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	inputs := strings.Split(string(fileContent), ",")
-	timers := make([]int, len(inputs))
+
+	initialTimers := make([]int, len(inputs))
 	for index, timer := range inputs {
-		timers[index], _ = strconv.Atoi(timer)
+		initialTimers[index], _ = strconv.Atoi(timer)
+	}
+
+	timerCounts := make(map[int]int)
+	for _, timerValue := range initialTimers {
+		timerCounts[timerValue]++
 	}
 
 	for i := 1; i <= *daysPtr; i++ {
-		var newTimers []int
-		for index, timer := range timers {
-			if timer == 0 {
-				timer = 6
-				newTimers = append(newTimers, 8)
-			} else {
-				timer--
-			}
-			timers[index] = timer
-		}
-		timers = append(timers, newTimers...)
+		timerCounts = calculate_timers(timerCounts)
 	}
-	fmt.Println("Number of lanterns", len(timers))
+
+	var total int
+	for _, count := range timerCounts {
+		total += count
+	}
+	fmt.Println(total)
 }
