9a721c47e0e4695c3db838748ff8386c2cb00118
Alex Archer <archeralex@gmail.com>
2021-12-07 14:24:07 +0000

    Day 7: implementation for part 1

---
+++ day7/day7.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	filePtr := flag.String("file", "input.txt", "input file location")
+	flag.Parse()
+
+	contents, _ := os.ReadFile(*filePtr)
+	inputs := strings.Split(string(contents), ",")
+
+	initialPositions := make([]int, len(inputs))
+	smallestPosition := initialPositions[0]
+	var largestPosition int
+	for index, input := range inputs {
+		inputInt, _ := strconv.Atoi(input)
+		if inputInt > largestPosition {
+			largestPosition = inputInt
+		} else if inputInt < smallestPosition {
+			smallestPosition = inputInt
+		}
+		initialPositions[index] = inputInt
+	}
+
+	smallestTotalSteps := math.Inf(1)
+	for position := smallestPosition; position <= largestPosition; position++ {
+		var totalSteps float64
+		for _, crabPosition := range initialPositions {
+			totalSteps += math.Abs(float64(crabPosition - position))
+		}
+		if totalSteps < smallestTotalSteps {
+			smallestTotalSteps = totalSteps
+		}
+	}
+	fmt.Println(smallestTotalSteps)
+}
