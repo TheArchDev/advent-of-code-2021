1d7b43e1abe7a8689856af86136d0e7b3081d3f3
Alex Archer <archeralex@gmail.com>
2021-12-07 16:01:36 +0000

    Day 7: first implementation of part 2

--- day7/day7.go
+++ day7/day7.go
@@ -23,6 +23,21 @@ func part_one(initialPositions []int, smallestPosition, largestPosition int) int
 	return int(smallestTotalSteps)
 }
 
+func part_two(initialPositions []int, smallestPosition, largestPosition int) int {
+	smallestTotalFuel := math.Inf(1)
+	for position := smallestPosition; position <= largestPosition; position++ {
+		var totalFuel float64
+		for _, crabPosition := range initialPositions {
+			positionDifference := math.Abs(float64(crabPosition - position))
+			totalFuel += ((positionDifference + 1) * positionDifference) / 2
+		}
+		if totalFuel < smallestTotalFuel {
+			smallestTotalFuel = totalFuel
+		}
+	}
+	return int(smallestTotalFuel)
+}
+
 func main() {
 	filePtr := flag.String("file", "input.txt", "input file location")
 	flag.Parse()
@@ -31,7 +46,7 @@ func main() {
 	inputs := strings.Split(string(contents), ",")
 
 	initialPositions := make([]int, len(inputs))
-	smallestPosition := initialPositions[0]
+	smallestPosition, _ := strconv.Atoi(inputs[0])
 	var largestPosition int
 	for index, input := range inputs {
 		inputInt, _ := strconv.Atoi(input)
@@ -44,4 +59,5 @@ func main() {
 	}
 
 	fmt.Println(part_one(initialPositions, smallestPosition, largestPosition))
+	fmt.Println(part_two(initialPositions, smallestPosition, largestPosition))
 }
